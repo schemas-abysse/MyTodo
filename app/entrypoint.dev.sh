5f98385d5e41fc7644a1d17ec82f1dd868071c4f
PC-PORT-DEV <pierre.mourret@protonmail.com>
2023-05-17 09:17:06 +0200

    add entrypoint.dev.sh

---
+++ app/entrypoint.dev.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Ce script permet de vérifier que le service de base de donnée est bien démarré au sein
+# du conteneur avant d'appliquer les migrations
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
